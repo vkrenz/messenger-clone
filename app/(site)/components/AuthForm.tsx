c562daa229e965b197fe1b47245d2126420e4965
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-14 23:39:03 -0400

    Update AuthForm.tsx

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,41 @@
 'use client';
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
     const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(() => {
+        if (variant === 'LOGIN') setVariant('REGISTER');
+        setVariant('LOGIN');
+    }, [variant]);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = data => {
+        setIsLoading(true);
+
+        if (variant === 'REGISTER') {
+            // Axios Register
+        }
+    }
+
     return (
         <div>
             Auth Form 2
